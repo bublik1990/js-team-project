eccf4b212d81bca4f6516fb4261f331bd07fc255
bublik1990 <bulanaja.julia@gmail.com>
2021-11-18 11:34:17 +0200

    project settings

--- src/index.js
+++ src/index.js
@@ -1,1 +1,9 @@
 import './sass/main.scss';
+
+import './js/main';
+import './js/search';
+import './js/pagination';
+import './js/modal';
+import './js/team-modal';
+import './js/add-to-queue';
+import './js/add-to-watched';
---
+++ src/js/library.js
@@ -0,0 +1,8 @@
+import '../sass/library.scss';
+
+import '../js/user-library';
+import '../js/pagination';
+import '../js/modal';
+import '../js/team-modal';
+import '../js/add-to-queue';
+import '../js/add-to-watched';
