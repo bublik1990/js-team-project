8a6176fefd055bd9c84e176487554e7c2054a397
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2021-11-22 17:31:00 +0200

    home-api

--- src/js/home.js
+++ src/js/home.js
@@ -1,6 +1,6 @@
-import filmCardTpl from '../partials/templates/filmCardlist-tmpl.hbs';
+import filmCardTpl from '../partials/templates/nrfilmCardlist-tmpl.hbs';
 const ANATOLII_API_KEY = '6ab460452e9d6fb8f59cab399bd5ef0f';
-const homeFilmsListRef = document.querySelector('.home__filmslist');
+const homeFilmsListRef = document.querySelector('.film-gallery');
 
 export function getPopularFilms() {
     fetch(`https://api.themoviedb.org/3/trending/movie/week?api_key=${ANATOLII_API_KEY}`)
