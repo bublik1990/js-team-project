988d671c7437760cb653dd04983a7e24860c50e0
Anatolii Litynskyi <litinskiy.tolik@gmail.com>
2021-11-30 14:28:36 +0200

    add refs.js

--- src/js/api-service.js
+++ src/js/api-service.js
@@ -1,5 +1,6 @@
+import getRefs from './refs'
+const refs = getRefs();
 const ANATOLII_API_KEY = '6ab460452e9d6fb8f59cab399bd5ef0f';
-const API_URL = 'https://api.themoviedb.org/3'
 
 
 export default class fetchFilms {
@@ -9,13 +10,17 @@ export default class fetchFilms {
     }
 
     async getPopularFilms() {
-        const response = await fetch(`${API_URL}/trending/movie/week?api_key=${ANATOLII_API_KEY}`)
-        const filmsCollection = response.json();
-        return filmsCollection;
+        try {
+            const response = await fetch(`${refs.API_URL}/trending/movie/week?api_key=${ANATOLII_API_KEY}`)
+            const filmsCollection = response.json();
+            return filmsCollection;
+        } catch (err) {
+            throw err;
+        }
     }
 
     async getSearchFilms() {
-        const response = await fetch(`${API_URL}/search/movie?api_key=${ANATOLII_API_KEY}&language=en-US&query=${this.searchQuery}&page=${this.page}&include_adult=false`);
+        const response = await fetch(`${refs.API_URL}/search/movie?api_key=${ANATOLII_API_KEY}&language=en-US&query=${this.searchQuery}&page=${this.page}&include_adult=false`);
         const filmsCollection = response.json();
         this.incrementPage();
         
