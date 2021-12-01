8f106c3a67ba939205d87affa96f6b3e4130cd05
Oleh Solomka <olehsolomka@gmail.com>
2021-12-01 13:51:20 +0200

    added markup-fix to pagination

--- src/js/home.js
+++ src/js/home.js
@@ -1,14 +1,14 @@
 import filmCardTpl from '../partials/templates/filmCardlist-tmpl.hbs';
-import filmCardOnSearchTpl from '../partials/templates/filmCardlist-onSearch-tmpl.hbs'
+import filmCardOnSearchTpl from '../partials/templates/filmCardlist-onSearch-tmpl.hbs';
 import fetchFilms from './api-service.js';
 import { toFixCardMarkup } from './markup-service.js';
-import { spinnerShow } from './spinner'
+import { spinnerShow } from './spinner';
 const API = new fetchFilms();
 
 const galleryRef = document.querySelector('.film-gallery');
 const searchFormRef = document.querySelector('.header__form');
 const searchErrorMes = document.querySelector('.header__search-error');
-const pagContainer = document.querySelector('.pagination__container')
+const pagContainer = document.querySelector('.pagination__container');
 
 export function clearGalleryMarkup() {
   galleryRef.innerHTML = '';
@@ -26,7 +26,6 @@ async function onSearch(e) {
   const form = e.currentTarget;
   const searchQuery = form.elements.query.value.toLowerCase().trim();
 
-
   if (!searchQuery) {
     // ----- Ошибка, если запрос пустой
     searchErrorMes.classList.remove('is-hidden');
@@ -37,7 +36,7 @@ async function onSearch(e) {
     arrayOfMovies = [];
   }
 
-  await collectFilms(form, searchQuery)
+  await collectFilms(form, searchQuery);
 
   // ----- Пришла одна страница, спрятать пагинацию
   // if (filmsCollection.total_pages === 1) {
@@ -45,34 +44,32 @@ async function onSearch(e) {
 }
 
 async function collectFilms(form, searchQuery) {
-
   API.query = searchQuery;
   clearGalleryMarkup();
   hiddenErrorMes();
   spinnerShow(galleryRef);
   const filmsCollection = await API.getSearchFilms();
-  //пушу колекцию 
-  arrayOfMovies.push(filmsCollection.results)
-  console.log(filmsCollection.results)
+  //пушу колекцию
+  arrayOfMovies.push(filmsCollection.results);
   appendSearchFilmsMarkup(arrayOfMovies);
   toFixCardMarkup();
 
   if (arrayOfMovies[API.returnPage() - 2].length !== 0) {
-    await collectFilms(form, searchQuery)
+    await collectFilms(form, searchQuery);
     //пока функии не вернут масив без фильмов, она будет просить ещё
   } else {
     //когда функция получила все фильмы то page сбрасывется, удаляется последний пустой масив и создается разметка
     API.resetPage();
-    arrayOfMovies.pop()
-    createPagMrkp()
-    return 
+    arrayOfMovies.pop();
+    createPagMrkp();
+    return;
   }
 }
 
-const list = document.querySelector(".pag-ul");
+const list = document.querySelector('.pag-ul');
 function createPagMrkp() {
   list.innerHTML = createPagination(arrayOfMovies.length - 1, 1, list);
-  addListenerToPag(arrayOfMovies.length - 1, 1, list)
+  addListenerToPag(arrayOfMovies.length - 1, 1, list);
   searchErrorMes.classList.add('is-hidden');
 
   // ----- Пришла одна страница, спрятать пагинацию
@@ -89,12 +86,12 @@ export function hiddenErrorMes() {
 }
 
 // ----- Слушатель на кнопке поиска
-searchFormRef.addEventListener('submit', (e) => {
-    e.preventDefault();
-    onSearch(e);
-})
+searchFormRef.addEventListener('submit', e => {
+  e.preventDefault();
+  onSearch(e);
+});
 
-const headerHomeBtn = document.querySelector('.header__home-page')
+const headerHomeBtn = document.querySelector('.header__home-page');
 
 // ----- Слушатель на кнопке Home
 for (let i = 0; i < headerHomeBtn.length; i++) {
@@ -104,14 +101,14 @@ for (let i = 0; i < headerHomeBtn.length; i++) {
     clearGalleryMarkup();
     spinnerShow(galleryRef);
     appendPopularFilmsMarkup();
-  })
+  });
 }
 
-const headerLibraryBtn = document.querySelector('.header__library-page')
+const headerLibraryBtn = document.querySelector('.header__library-page');
 
 headerLibraryBtn.addEventListener('click', () => {
-  list.innerHTML = ''
-})
+  list.innerHTML = '';
+});
 
 // ----- Слушатель на кнопке Library
 for (let i = 0; i < headerLibraryBtn.length; i++) {
@@ -119,17 +116,18 @@ for (let i = 0; i < headerLibraryBtn.length; i++) {
     headerHomeBox.classList.add('is-inactive');
     headerLibraryBox.classList.remove('is-inactive');
     clearGalleryMarkup();
-  })
+  });
 }
 
 function createPagination(totalPages, page, list) {
-  page = Number(page)
+  page = Number(page);
   let liTag = '';
   let active;
   let beforePage = page - 1;
   let afterPage = page + 1;
 
-  if (page > 1) { //show the next button if the page value is greater than 1
+  if (page > 1) {
+    //show the next button if the page value is greater than 1
     liTag += `<li class="prev"><svg id="prev-svg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M12.6666 8H3.33325" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M7.99992 12.6667L3.33325 8.00004L7.99992 3.33337" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
@@ -137,32 +135,32 @@ function createPagination(totalPages, page, list) {
 </li>`;
   }
 
-  if (page > 2) { //if page value is less than 2 then add 1 after the previous button
+  if (page > 2) {
+    //if page value is less than 2 then add 1 after the previous button
     liTag += `<li class="first numb">1</li>`;
-    if (page > 3) { //if page value is greater than 3 then add this (...) after the first li or page
+    if (page > 3) {
+      //if page value is greater than 3 then add this (...) after the first li or page
       liTag += `<li class="dots">...</li>`;
     }
   }
 
-    if (page == totalPages) {
-      beforePage = beforePage - 2;
-    }
-    if (totalPages == 1) {
-      beforePage = 0;
-    }
-    if (page == totalPages - 1) {
-      beforePage = beforePage - 1;
-    }
-    if (totalPages == 2) {
-      beforePage = 1
-    }
+  if (page == totalPages) {
+    beforePage = beforePage - 2;
+  }
+  if (totalPages == 1) {
+    beforePage = 0;
+  }
+  if (page == totalPages - 1) {
+    beforePage = beforePage - 1;
+  }
+  if (totalPages == 2) {
+    beforePage = 1;
+  }
 
   // how many pages or li show after the current li
   if (page == 1) {
     afterPage = afterPage + 2;
-
   } else if (page == 2) {
-
     afterPage = afterPage + 1;
   }
   // else if (page == 3) {
@@ -170,65 +168,74 @@ function createPagination(totalPages, page, list) {
   // }
 
   for (var plength = beforePage; plength <= afterPage; plength++) {
-
-    if (plength > totalPages) { //if plength is greater than totalPage length then continue
+    if (plength > totalPages) {
+      //if plength is greater than totalPage length then continue
       continue;
     }
-    if (plength == 0) { //if plength is 0 than add +1 in plength value
+    if (plength == 0) {
+      //if plength is 0 than add +1 in plength value
       plength = plength + 1;
     }
-    if(page == plength){ //if page is equal to plength than assign active string in the active variable
-      active = "active";
-    }else{ //else leave empty to the active variable
-      active = "";
+    if (page == plength) {
+      //if page is equal to plength than assign active string in the active variable
+      active = 'active';
+    } else {
+      //else leave empty to the active variable
+      active = '';
     }
     liTag += `<li class="numb ${active}">${plength}</li>`;
   }
-  if(page < totalPages - 1){ //if page value is less than totalPage value by -1 then show the last li or page
-    if(page < totalPages - 2){ //if page value is less than totalPage value by -2 then add this (...) before the last li or page
+  if (page < totalPages - 1) {
+    //if page value is less than totalPage value by -1 then show the last li or page
+    if (page < totalPages - 2) {
+      //if page value is less than totalPage value by -2 then add this (...) before the last li or page
       liTag += `<li class="dots">...</li>`;
     }
     liTag += `<li class="last numb">${totalPages}</li>`;
   }
-  if (page < totalPages) { //show the next button if the page value is less than totalPage(20)
-      liTag += `<li class="next"><svg id="next-svg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
+  if (page < totalPages) {
+    //show the next button if the page value is less than totalPage(20)
+    liTag += `<li class="next"><svg id="next-svg" width="16" height="16" viewBox="0 0 16 16" fill="none" xmlns="http://www.w3.org/2000/svg">
 <path d="M3.33341 8H12.6667" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
 <path d="M8.00008 12.6667L12.6667 8.00004L8.00008 3.33337" stroke="black" stroke-width="1.33333" stroke-linecap="round" stroke-linejoin="round"/>
 </svg>
 </li>`;
   }
-       
+
   list.innerHTML = liTag; //add li tag inside ul tag
   return liTag; //reurn the li tag
 }
 
 function addListenerToPag(totalPages, page, list) {
- page = Number(page)
-  
-  list.addEventListener('click', (e) => {
-  if (e.target.className == 'next') {
-    createPagination(totalPages, page += 1, list)
-    galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
-    }
-  if (e.target.id == 'next-svg') {
-    createPagination(totalPages, page += 1, list)
-    galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
-    }
-  if (e.target.id == 'prev-svg') {
-    createPagination(totalPages, page -= 1, list)
-    galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
-    }
-  if (e.target.className == 'prev') {
-    createPagination(totalPages, page -= 1, list)
-    galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
-    } 
-  if (e.target.classList.contains('numb')) {
-    createPagination(totalPages, e.target.innerHTML, list)
-    page = Number(e.target.innerHTML);
-    console.log(page);
-    galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
-    }
-  })
-  
-}
+  page = Number(page);
 
+  list.addEventListener('click', e => {
+    if (e.target.className == 'next') {
+      createPagination(totalPages, (page += 1), list);
+      galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
+      toFixCardMarkup();
+    }
+    if (e.target.id == 'next-svg') {
+      createPagination(totalPages, (page += 1), list);
+      galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
+      toFixCardMarkup();
+    }
+    if (e.target.id == 'prev-svg') {
+      createPagination(totalPages, (page -= 1), list);
+      galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
+      toFixCardMarkup();
+    }
+    if (e.target.className == 'prev') {
+      createPagination(totalPages, (page -= 1), list);
+      galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
+      toFixCardMarkup();
+    }
+    if (e.target.classList.contains('numb')) {
+      createPagination(totalPages, e.target.innerHTML, list);
+      page = Number(e.target.innerHTML);
+      console.log(page);
+      galleryRef.innerHTML = filmCardOnSearchTpl(arrayOfMovies[page]);
+      toFixCardMarkup();
+    }
+  });
+}
